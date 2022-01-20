d7997e67acf33eacfc0e3aff9a3d2d97cb453349
JosephAnderson234 <coserojasj@gmail.com>
2022-01-20 14:21:08 -0500

    test1

--- mainI/mainI.py
+++ mainI/mainI.py
@@ -1,7 +1,7 @@
 from instalador.instalador import Installer
 from delete.eliminador import Eliminador
 
-url="https://github.com/Programadoresdel3D/Program/archive/refs/heads/main.zip"
+url="https://github.com/JosephAnderson234/Program/archive/refs/heads/main.zip"
 file = "temporal.zip"
 
 Installer(url, file)
